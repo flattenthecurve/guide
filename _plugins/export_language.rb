d929f26cef6eba706972eefdbd7a568d8e085f09
Juan Wajnerman <waj@manas.tech>
2020-03-16 17:38:25 -0300

    Export pages content and strings to locakise

--- _plugins/export_language.rb
+++ _plugins/export_language.rb
@@ -1,7 +1,20 @@
 require 'json'
 
-Jekyll::Hooks.register :site, :post_write do |site|
+texts = {}
+
+Jekyll::Hooks.register :site, :pre_render do |site|
   texts = {}
+end
+
+Jekyll::Hooks.register :pages, :post_init do |page|
+  if key = key_from_page_filename(page.name)
+    texts[key] = page.content
+  end
+end
+
+Jekyll::Hooks.register :site, :post_write do |site|
+  next unless site.config['active_lang'] == 'en'
+  texts.merge!(site.data['strings'])
 
   site.data['sections']['en'].each do |file|
     key = key_from_filename(file)
