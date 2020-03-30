93ecddfd4608f6640f4c46c49bd6e66a2cfca25d
Jan Rous <rousik@gmail.com>
2020-03-30 13:30:10 -0600

    Resource automation (#248)
    * Prototype of resource autgen script.
    
    * Minor improvements to the resource importer.
    
    1. Improved MD file naming.
    Allow for longer strings (35 chars), use hyphens instead of underscores
    and remove sequences of hyphens and trailing hyphens.
    
    2. Enable resource verification column
    Existing resources are now marked with "legacy" in the corresponding
    column so this works well.
    
    3. Regenerated all resource files.
    Right now, if there's change to the naming logic, old files remain in place.
    It might be worth exploring some form of --nuke-old-content flag that will
    remove all preexisting resources (at least those with timestamp prefix).
    
    * Rework import_resources script
    
    1. fetch public csv feed of the resources, this doesn't need
    any auth and is much simpler.
    2. use csv.DictReader which simplifies data handling logic
    as well.
    3. Do not synchronize country names. We can sanitize content
    of the spreadsheet instead.
    
    * Reverting changes to resources.
    
    * Revert changes to .gitignore

---
+++ _scripts/import_resources.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+import csv
+import requests
+import argparse
+import datetime
+import os
+import re
+
+# Resource spreadsheet contents are published at this URL in the form of CSV
+RESOURCES_URL="https://docs.google.com/spreadsheets/d/e/2PACX-1vQMEdZXKgYNybkqNv4X26CVNoQZHuE0zb27wuBDgdDwtiyWCICQLhyU_LuLJVeHD5oTRnp-bEdFTuqi/pub?gid=650653420&single=true&output=csv"
+
+parser = argparse.ArgumentParser(description='Parse arguments for this script.')
+parser.add_argument('--dryrun', '-n', default=False, action="store_true")
+args = parser.parse_args()
+"""Commandline argument values are stored here."""
+
+REQUIRED_COLUMNS = ['name', 'category', 'description']
+"""These columns must be non-empty for the row to be accepted."""
+
+EXTRACT_ATTRIBUTES = ['name', 'category', 'country', 'state']
+"""Columns that will be added into MD file as attributes, in this order."""
+
+
+def make_row_filename(row):
+    """Constructs standardized base filename for this resource.
+
+    Adds timestamp prefix, lowercased alphanumeric character from the title
+    are retained, spaces replaced with dashes and total length of title
+    limited at 35 characters.
+
+    Returns the resulting base filename.
+    """
+    sanitized_name = row['name'].lower().replace(" ", "-")
+    sanitized_name = re.sub(r'[^-a-zA-Z0-9]', '', sanitized_name)
+    sanitized_name = re.sub(r'--*', '-', sanitized_name)
+    sanitized_name = sanitized_name[:35].strip("-")
+    dt = datetime.datetime.strptime(
+        row['timestamp'].split()[0],
+        '%m/%d/%Y').date()
+    return f'{dt.isoformat()}-{sanitized_name}.md'
+
+   
+def format_row_contents(row):
+    """Generate MD output for each row."""
+    attr_rows = []
+    for attr in EXTRACT_ATTRIBUTES:
+        if not row.get(attr, None):
+            continue
+        # TODO: ensure special character escaping.
+        attr_rows.append(f'{attr}: {row[attr]}')
+    all_attributes = '\n'.join(attr_rows)
+    return (
+        f"---\n"
+        f"{all_attributes}\n"
+        f"\nURL: {row['url'].strip()}\n"
+        f"---\n"
+        f"\n"
+        f"{row['description']}")
+
+
+def get_resource_dir():
+    """Finds _resources/ directory or terminates this script."""
+    for res in ['_resources/', '../_resources']:
+        if os.path.isdir(res):
+            return res
+    sys.exit("Can't find _resources/ directory.")
+    return None
+
+
+def main():
+    resource_dir = get_resource_dir()
+    rows = csv.DictReader(requests.get(RESOURCES_URL).iter_lines(decode_unicode=True))
+    rows_accepted = 0
+    for row_num, row in enumerate(rows, start=2):
+        # Check that all required columns are present
+        has_required = True
+        for reqd in REQUIRED_COLUMNS:
+            if not row.get(reqd, None):
+                print("Row {row_num} skipped. It is missing required column '{reqd}'.")
+                has_required = False
+                break
+        if not has_required:
+            continue
+
+        rows_accepted += 1
+        filename = f'{resource_dir}{make_row_filename(row)}'
+        
+        if args.dryrun:
+            print(f"*** Dry run *** {filename} would contain:")
+            print(format_row_contents(row))
+        else:
+            with open(filename, 'w+') as out_file:
+                out_file.write(format_row_contents(row))
+                out_file.close()
+
+    print(f'Imported {rows_accepted} resources.')
+        
+
+if __name__ == '__main__':
+    main()
